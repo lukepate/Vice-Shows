8fe8aeafe2a68e47c691043b468762ccc8f4dea1
lukepate <lukewpate@gmail.com>
2021-06-30 14:19:47 -0400

    remove depends from app spec

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import "@testing-library/jest-dom";
 import { render, waitFor } from '@testing-library/react';
 import App from './App';
-import Dashboard from './Dashboard';
 import { showsData } from '../mockdata';
 import nock from 'nock';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import fetch from 'node-fetch';
-import useFetch from "react-fetch-hook";
 import "whatwg-fetch";
 
 
@@ -21,7 +17,6 @@ test('shows render', async () => {
         data: showsData
       })
     const app = render(<App />);
-
     await waitFor(() => expect(app).toBeTruthy());
 });
 
